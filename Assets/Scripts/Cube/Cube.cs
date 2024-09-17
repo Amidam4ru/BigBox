a6ddf4d366431764afa964bb14676de34204efb6
Amidam4ru <uraxatakov@gmail.com>
2024-09-17 17:50:22 +0300

    3commit

--- Assets/Scripts/Cube/Cube.cs
+++ Assets/Scripts/Cube/Cube.cs
@@ -1,12 +1,5 @@
 using UnityEngine;
 
-[RequireComponent(typeof(CrushingCube))]
-[RequireComponent(typeof(Collider))]
-[RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(FallingOut))]
-[RequireComponent(typeof(Renderer))]
-[RequireComponent(typeof(Rigidbody))]
-
 public class Cube : MonoBehaviour
 {
 
